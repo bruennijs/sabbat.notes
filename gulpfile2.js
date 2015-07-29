39bafd815fd4da664c384d1be75c0a99766f626d
bruenni <oliver.bruentje@gmx.de>
2015-07-29 18:53:38 +0200

    Gulp connect implemnted but no reload in browser yet

--- gulpfile2.js
+++ gulpfile2.js
@@ -1,42 +1,33 @@
 var gulp = require("gulp");
-var print = require("gulp-print")
-var console = require("console")
-var connect = require("gulp-connect")
-var watch = require("gulp-watch")
+var print = require("gulp-print");
+var console = require("console");
+var connect = require("gulp-connect");
+var watch = require("gulp-watch");
 
-var srcDirsUi = ['sabbat.notes.ui/*.html', 'sabbat.notes.ui/css/*.css'];
-var srcDirsScripts = ['sabbat.notes.ui/scripts/*.js'];
+var srcDirsUi = ['sabbat.notes.ui/*.html', 'sabbat.notes.ui/css/*.css', 'sabbat.notes.ui/scripts/*.js'];
 
 gulp.task('serve', function() {
     console.info('serve started...');
-
-    stream = gulp.src(srcDirsUi)
-        .pipe(print())
-        .pipe(connect.server(
-            {
-                port:8000,
-                livereload: true,
-            }));
+    connect.server(
+        {
+            root:'sabbat.notes.ui',
+            port:8000,
+            livereload: true
+        });
 });
 
-gulp.task('livereload', function() {
-    gulp.src(srcDirsUi.concat(srcDirsScripts))
+gulp.task('reload', function () {
+    gulp.src(srcDirsUi)
         .pipe(print())
-        .pipe(watch())
-        .pipe(print())
-        .pipe(connect.reload())
-})
+        .pipe(connect.reload());
+});
 
 gulp.task('typescript', function() {
     console.info("compile changes");
-})
+});
 
 gulp.task('watch', function() {
-    gulp.watch(srcDirsScripts, ['typescript']);
-})
+    gulp.watch(srcDirsUi, ['reload']);
+});
 
-gulp.task('stop', function() {
-    webserver.reload;
-})
-
-gulp.task('default', ['serve', 'watch', 'livereload']);
+gulp.task('default', ['serve', 'watch']);
