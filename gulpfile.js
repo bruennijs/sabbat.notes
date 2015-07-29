c362b23bd9af6e6d2dd55d00fdb5401afbdda7cc
bruenni <oliver.bruentje@gmx.de>
2015-07-29 17:52:50 +0200

    added new gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,23 +1,36 @@
 var gulp = require("gulp");
+var print = require("gulp-print")
 var console = require("console")
 var webserver = require("gulp-webserver")
+var glob = require('glob')
 
 gulp.task('default', ['serve']);
 
 gulp.task('serve', function() {
     console.info('serve started...')
 
-    stream = gulp.src('/usr/local/src/git/sabbat.notes/sabbat.notes.ui')
+    stream = gulp.src(["sabbat.notes.ui/*.html", "sabbat.notes.ui/css/*.css"])
+        .pipe(print(function(path) {
+            console.info(path);
+        }))
         .pipe(webserver(
             {
                 port:8000,
                 livereload: true,
                 directoryListing: true,
-                open:'http://127.0.0.1:8000/index.html',
+                open:'http://localhost:8000/index.html',
                 //path:'public',
                 https:false}));
 });
 
 gulp.task('stop', function() {
+    webserver.reload;
+})
 
+gulp.task('dirs', function() {
+    glob("**", {ignore: "node_modules"}, function(err, files) {
+        files.forEach(function(val, idx) {
+            console.info(val + "[" + idx + "]")
+        })
+    })
 })
---
+++ gulpfile2.js
@@ -0,0 +1,42 @@
+var gulp = require("gulp");
+var print = require("gulp-print")
+var console = require("console")
+var connect = require("gulp-connect")
+var watch = require("gulp-watch")
+
+var srcDirsUi = ['sabbat.notes.ui/*.html', 'sabbat.notes.ui/css/*.css'];
+var srcDirsScripts = ['sabbat.notes.ui/scripts/*.js'];
+
+gulp.task('serve', function() {
+    console.info('serve started...');
+
+    stream = gulp.src(srcDirsUi)
+        .pipe(print())
+        .pipe(connect.server(
+            {
+                port:8000,
+                livereload: true,
+            }));
+});
+
+gulp.task('livereload', function() {
+    gulp.src(srcDirsUi.concat(srcDirsScripts))
+        .pipe(print())
+        .pipe(watch())
+        .pipe(print())
+        .pipe(connect.reload())
+})
+
+gulp.task('typescript', function() {
+    console.info("typescript compiling");
+})
+
+gulp.task('watch', function() {
+    gulp.watch(srcDirsScripts, ['typescript']);
+})
+
+gulp.task('stop', function() {
+    webserver.reload;
+})
+
+gulp.task('default', ['serve', 'watch', 'livereload']);
